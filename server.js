8065ecba56bd1f8578bfaaea7f65a9b65fd88383
Will <113927655+willmiles00@users.noreply.github.com>
2023-11-21 10:59:34 -0700

    uh

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-https://runkit.com/
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`serverside on port ${port}`)
 })
