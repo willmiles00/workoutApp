0be843ddd397d0508bf0fa1d5ff10e5e94f4411b
Will <113927655+willmiles00@users.noreply.github.com>
2023-12-01 14:49:28 -0700

    got workouts to push to timeline

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ let trackBtn = document.querySelector('#trackBtn')
 let addNewWorkoutPopout = document.querySelector('#addNewWorkoutPopout')
 let newWorkoutForm = document.querySelector('#newWorkoutForm')
 let workoutSelect = document.querySelector('#workoutSelect')
-let previousWorkoutsSection = document.querySelector("#previousWorkoutsSection")
+let workoutTimeline = document.querySelector('#workoutTimeline')
 
 
 let completedWorkouts = [
@@ -152,16 +152,26 @@ function viewWorkouts(completedWorkouts){
 completedWorkouts.sort((a, b) => new Date(b.date) - new Date(a.date));
 
   // clears out previously submitted workouts
-  previousWorkoutsSection.innerHTML = "";
+  workoutTimeline.innerHTML = "";
 
 completedWorkouts.forEach((workout)=>{
   console.log(workout)
 
-      const p = `<p data-todoText='${workout.workout}'>
-    <b>Date:</b> <div class="workoutDate">${workout.date}<div> <b>Workout:</b> ${workout.workout} <b>Details:</b>
-</p> `;
+  
+// builds a new item on the Workout Timeline
+const li= `<li>
+<div class="timeline-middle">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
+</div>
+<div class="timeline-start md:text-end mb-10">
+  <time class="font-mono italic">${workout.date}</time>
+  <div class="text-lg font-black">Workout: ${workout.workout}</div>
+  details: fdasnklnfldas
+</div>
+<hr/>
+</li>` 
 
-    previousWorkoutsSection.insertAdjacentHTML("beforeend", p);
+    workoutTimeline.insertAdjacentHTML("beforeend", li);
 
 })
 }
