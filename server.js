20f54675d92b362d141f77867d0282fd0e2de498
Will <113927655+willmiles00@users.noreply.github.com>
2023-12-09 15:58:12 -0700

    started backend

--- server.js
+++ server.js
@@ -2,11 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser = require('body-parser')
+
+let completedWorkouts = [
+]
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`serverside on port ${port}`)
-})
+app.post('/', (req, res) => {
 
+  completedWorkouts.push('books')
+
+  res.send(completedWorkouts)
+}
+)
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
